207a6b8eb96217b2e6fb21b644a7cd6b9fd36b4d
Marco Emme <duemme.mail@gmail.com>
2026-08-25 17:59:23 +0200

    chore: rimosso Apify fallback e completato passaggio a SocialCrawl [deployvps]

--- test_sc.php
+++ test_sc.php
@@ -1,8 +1,12 @@
 <?php
 require_once 'config/config.php';
 require_once 'config/keys.php';
-$payload = ["startUrls" => [["url" => "https://www.facebook.com/zuck"]], "resultsLimit" => 2];
-$url = "https://api.socialcrawl.dev/facebook/profile";
+
+$payload = [
+    'inputUrl' => 'https://www.instagram.com/zuck/',
+    'maxPosts' => 2
+];
+$url = "https://api.socialcrawl.dev/instagram/posts";
 $ch = curl_init($url);
 curl_setopt_array($ch, [
     CURLOPT_RETURNTRANSFER => true,
@@ -12,7 +16,20 @@ curl_setopt_array($ch, [
         'x-api-key: ' . SOCIALCRAWL_API_KEY
     ],
     CURLOPT_POSTFIELDS     => json_encode($payload),
+    CURLOPT_TIMEOUT        => 90,
+    CURLOPT_SSL_VERIFYPEER => true,
+    CURLOPT_SSL_VERIFYHOST => 2,
 ]);
+$startTime = microtime(true);
 $res = curl_exec($ch);
+$elapsed = round(microtime(true) - $startTime, 2);
+$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
-echo "Response: " . substr($res, 0, 1000);
+
+echo "HTTP Code: $code\n";
+echo "Elapsed: {$elapsed}s\n";
+if ($res) {
+    echo "Response: " . substr($res, 0, 2000) . "\n";
+} else {
+    echo "No response.\n";
+}
