d6e8d5764ee0bae1119dc735a5bf2e91ef12288c
Marco Emme <duemme.mail@gmail.com>
2026-09-26 17:54:44 +0200

    Cleanup migration script

--- api/migrate_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/db.php';
-try {
-    DB::execute("ALTER TABLE sites ADD COLUMN declared_strategy longtext DEFAULT NULL");
-    echo "Migration OK";
-} catch (Throwable $e) {
-    echo "Migration failed: " . $e->getMessage();
-}
